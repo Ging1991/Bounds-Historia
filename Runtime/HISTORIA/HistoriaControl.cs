8b199dba26d01e68f9b90fb759ca887f46d19589
Ging1991 <carloscaballeromorel@gmail.com>
2026-03-03 00:54:45 -0300

    Commit inicial

---
+++ Runtime/HISTORIA/HistoriaControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Bounds.Infraestructura;
+using Ging1991.Dialogos;
+using Ging1991.Dialogos.Persistencia;
+using Ging1991.Persistencia.Direcciones;
+using Bounds.Dialogos;
+using Bounds.Persistencia;
+using Bounds.Persistencia.Parametros;
+using Bounds.Modulos.Persistencia;
+
+namespace Bounds.Historia {
+
+	public class HistoriaControl : MonoBehaviour {
+
+		public Dialogo<AccionBounds> dialogo;
+		private Configuracion configuracion;
+		public ParametrosControl parametrosControl;
+		public MusicaDeFondo musicaDeFondo;
+
+		void Start() {
+			parametrosControl.Inicializar();
+			ParametrosEscena parametros = parametrosControl.parametros;
+			configuracion = new(parametros.direcciones["CONFIGURACION"]);
+			musicaDeFondo.Inicializar(parametros.direcciones["MUSICA_TIENDA"]);
+
+			DireccionRecursos direccion = new("HISTORIA", $"CAPITULO{configuracion.LeerCapituloHistoria()}");
+
+			LectorLista<AccionBounds> lector1 = new LectorLista<AccionBounds>(
+				direccion.Generar(),
+				Ging1991.Persistencia.Lectores.TipoLector.RECURSOS
+			);
+
+			dialogo.Inicializar(
+				lector1.GetLista(),
+				new LectorImagenes(new DireccionRecursos("PERSONAJES/MINIATURAS")),
+				new LectorImagenes(new DireccionRecursos("PERSONAJES/AVATARES"))
+			);
+
+		}
+
+
+		public void BotonVolver() {
+			ControlEscena escena = ControlEscena.GetInstancia();
+			escena.CambiarEscena_menu();
+		}
+
+
+	}
+
+}
